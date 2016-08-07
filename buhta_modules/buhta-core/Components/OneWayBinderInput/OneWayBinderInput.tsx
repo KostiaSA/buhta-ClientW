b907e685e2d6247a1a4344e76351df27a54e6ae3
KostiaSA <savchenkov@buhta.ru>
2016-08-07 21:13:09 +0300

    desktop

--- buhta_modules/buhta-core/Components/OneWayBinderInput/OneWayBinderInput.tsx
+++ buhta_modules/buhta-core/Components/OneWayBinderInput/OneWayBinderInput.tsx
@@ -1,9 +1,13 @@
 import * as React from "react";
+import * as _ from "lodash";
 import {ComponentProps, Component, ComponentState} from "../Component";
 import {AutoFormControlProps} from "../AutoForm/AutoForm";
 import {OneWayBinder, getOneWayBinderTypesDataSource} from "../../../buhta-schema/OneWayBinder/OneWayBinder";
 import {SelectInput} from "../SelectInput/SelectInput";
 import {SelectInputDataSource} from "../SelectInput/SelectInputDataSource";
+import {OneWayBinder_StringValue} from "../../../buhta-schema/OneWayBinder/OneWayBinder_StringValue";
+import {OneWayBinder_NumberValue} from "../../../buhta-schema/OneWayBinder/OneWayBinder_NumberValue";
+import {throwError} from "../../Error";
 
 export interface OneWayBinderInputProps extends ComponentProps<any>, AutoFormControlProps {
     bindObject: any;
@@ -75,9 +79,20 @@ export class OneWayBinderInput extends Component<OneWayBinderInputProps, any> {
     private activeBinder: OneWayBinder<any>;
 
     render(): JSX.Element {
+        let binder = this.props.bindObject[this.props.bindPropName];
+        if (_.isString(binder)) {
+            binder = new OneWayBinder_StringValue(binder);
+            this.props.bindObject[this.props.bindPropName]=binder;
+        }
+        else if (_.isNumber(binder)) {
+            binder = new OneWayBinder_NumberValue(binder);
+            this.props.bindObject[this.props.bindPropName]=binder;
+        }
+        else if (!(binder instanceof OneWayBinder))
+            throwError("OneWayBinderInput.render(): invalid edited value type");
 
         if (!this.activeBinder)
-            this.activeBinder = this.props.bindObject[this.props.bindPropName] as OneWayBinder<any>;
+            this.activeBinder = binder as OneWayBinder<any>;
 
         if (!this.selectDataSource)
             this.selectDataSource = getOneWayBinderTypesDataSource(this.activeBinder);
